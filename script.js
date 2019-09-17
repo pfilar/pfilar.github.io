de5e58c5d5deb335516c56bafd65193b50e469ff
Piotr Filarecki <pfilar2@wp.pl>
2019-09-17 17:19:17 +0200

    v3

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 ﻿$(document).ready(function() {
   const apiRoot = 'https://stormy-castle-16257.herokuapp.com/v1/task/';
-  const trelloApiRoot = 'https://stormy-castle-16257.herokuapp.com/v1/task/';
+  const trelloApiRoot = 'https://stormy-castle-16257.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
