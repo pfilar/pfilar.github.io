42232694a690be487d0d1235022e5b838c3e7b9c
Piotr Filarecki <pfilar2@wp.pl>
2019-09-17 11:04:10 +0200

    frondeend v2

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 ﻿$(document).ready(function() {
   const apiRoot = 'https://stormy-castle-16257.herokuapp.com/v1/task/';
-  const trelloApiRoot = 'http://localhost:8080/v1/trello/';
+  const trelloApiRoot = 'https://stormy-castle-16257.herokuapp.com/v1/task/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
